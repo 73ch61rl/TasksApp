ef04f9dc0045a12fd34c9e2459b4c226332386b7
Irina Koski <irina.smirnova86@gmail.com>
2024-02-29 08:56:11 +0200

    Fixing apiUrl variable.

--- tasksapp.client/src/app/services/task-list.service.ts
+++ tasksapp.client/src/app/services/task-list.service.ts
@@ -8,29 +8,29 @@ import { Task } from '../models/task.model';
 })
 export class TaskListService {
 
-  apiUrl: string = "http://localhost:5072";
+  apiUrl: string = "http://localhost:5072/api/tasks/";
 
   constructor(private http: HttpClient) { }
 
   getAllTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiUrl + '/api/tasks');
+    return this.http.get<Task[]>(this.apiUrl);
   }
 
   addTask(newTask: Task): Observable<Task> {
     newTask.id = '0';
-    return this.http.post<Task>(this.apiUrl + '/api/tasks', newTask);
+    return this.http.post<Task>(this.apiUrl, newTask);
   }
 
   getTask(id: string): Observable<Task> {
-    return this.http.get<Task>(this.apiUrl + '/api/tasks/' + id);
+    return this.http.get<Task>(this.apiUrl + id);
   }
 
   updateTask(id: string, updateTaskRequest: Task): Observable<Task> {
-    return this.http.put<Task>(this.apiUrl + '/api/tasks/' + id, updateTaskRequest);
+    return this.http.put<Task>(this.apiUrl + id, updateTaskRequest);
   }
 
   deleteTask(id: string): Observable<Task> {
-    return this.http.delete<Task>(this.apiUrl + '/api/tasks/' + id);
+    return this.http.delete<Task>(this.apiUrl + id);
   }
 
 }
